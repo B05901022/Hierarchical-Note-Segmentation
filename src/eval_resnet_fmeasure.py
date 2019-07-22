7017bf977c30a785a477118991adfb0a1e86346f
b05901022 <b05901022@ntu.edu.tw>
2019-07-22 09:35:44 +0800

    fix

--- src/eval_resnet_fmeasure.py
+++ src/eval_resnet_fmeasure.py
@@ -541,7 +541,7 @@ for step, xys in enumerate(input_loader):                 # gives batch data
             predict_on_notes.append([0.0 for j in range(OUTPUT_SIZE)])
 
     predict_on_notes_np = np.ndarray(shape=(len(predict_on_notes), OUTPUT_SIZE), dtype=float, buffer=np.array(predict_on_notes))
-    for i in range(int(predict_on_notes_np.shape[0]*0.1)):
+    for i in range(int(predict_on_notes_np.shape[0])):
         print(str(i),predict_on_notes_np[i])
     #onset_times, probseq_on_np = Smooth_prediction(predict_on_notes_np, THRESHOlD) # list of onset secs, ndarray
     #offset_times, probseq_off_np = Smooth_prediction(predict_off_notes_np, THRESHOlD) # list of onset secs, ndarray
