e25967d8e734c4c3c9854b14a0a9b42fe837a89d
b05901022 <b05901022@ntu.edu.tw>
2019-07-31 14:06:37 +0800

    9 channel

--- script/train_sdt6_resnet_top.sh
+++ script/train_sdt6_resnet_top.sh
@@ -8,13 +8,13 @@ SE=1 #5
 BIDIR1=1
 NORM=ln
 
-BATCH=8 #128 #32 #10
+BATCH=128 #128 #32 #10
 #FEAT1=SN_SF1_SIN_SF1_F6
 FEAT1=SN_SF1_SIN_SF1_ZN_F9
 FEAT_NUM1=9
 
 START_EPOCH=0 #
-END_EPOCH=40 #400 #80
+END_EPOCH=10 #400 #80
 
 LOG_FILE="log/sdt6_resnet_pyramidnet44shakedropfixoptim_${NORM}${WS}_l${HL1}h${HS1}b${BIDIR1}_e${END_EPOCH}b${BATCH}_${FEAT1}_sample.log"
 
