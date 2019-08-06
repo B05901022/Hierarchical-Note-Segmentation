00ce3315ca47cabce64a4361f646e969edd601de
b05901022 <b05901022@ntu.edu.tw>
2019-08-06 16:30:39 +0800

    DataAug_Exp

--- src/train_modules/train_sdt6_resnet_DataAug.py
+++ src/train_modules/train_sdt6_resnet_DataAug.py
@@ -79,13 +79,13 @@ def train_resnet_4loss_DataAug(input_t, target_Var, decoders, dec_opts, device,
     return totLoss / input_time_step
 
 def DataAug(labeled_data, labeled_label, device, 
-            transform_dict={'cutout'    :False, #{'n_holes':1, 'height':50, 'width':5}, 
+            transform_dict={'cutout'    :{'n_holes':1, 'height':50, 'width':5}, 
                             'freq_mask' :False, #{'freq_mask_param':100},
                             'time_mask' :False, #{'time_mask_param':5},
-                            'pitchshift':{'shift_range':48},
+                            'pitchshift':False, #{'shift_range':48},
                             'addnoise'  :False, #{'noise_type':'pink', 'noise_size':0.01}, 
                             },
-            MixUp_bool=False, beta_dist_alpha=0.75, # MixUp
+            MixUp_bool=True, beta_dist_alpha=0.75, # MixUp
             ):
     
     # labeled_data   shape: (10, 9, 174, 19)
