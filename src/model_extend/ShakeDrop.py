4b1f9f5927a162fbd5c459c87fc04e52364845c3
b05901022 <b05901022@ntu.edu.tw>
2019-08-13 13:57:46 +0800

    fix_label_guess

--- src/model_extend/ShakeDrop.py
+++ src/model_extend/ShakeDrop.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 import math
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 
 #######################################################################################
 # ShakeDrop Reference
