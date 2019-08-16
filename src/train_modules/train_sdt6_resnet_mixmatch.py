c41b7f5c4a8b11690a6dd0a0e3dedc4e86e8666c
b05901022 <b05901022@ntu.edu.tw>
2019-08-16 14:48:12 +0800

    MixUp_Fix

--- src/train_modules/train_sdt6_resnet_mixmatch.py
+++ src/train_modules/train_sdt6_resnet_mixmatch.py
@@ -21,7 +21,7 @@ from train_modules.audio_augment import transform_method
 
 def train_resnet_4loss_mixmatch(input_t, target_Var, decoders, dec_opts, device,
     loss_funcs, INPUT_SIZE, OUTPUT_SIZE, BATCH_SIZE, k,
-    unlabel_t, unlabel_lambda=75.0,
+    unlabel_t, unlabel_lambda=100.0,
     ):
     
     # input_t    shape: (1,3,522,data_length)
@@ -80,7 +80,7 @@ def train_resnet_4loss_mixmatch(input_t, target_Var, decoders, dec_opts, device,
         onDecOut4 = torch.cat((onDecOut1, temp_t), dim=1)
         
         # === Unlabeled ===
-        """
+        
         onDecOut6_u = onDec(u_mix_data)
         onDecOut1_u = nn_softmax(onDecOut6_u[:, :2])
         onDecOut2_u = nn_softmax(onDecOut6_u[:, 2:4])
@@ -89,7 +89,7 @@ def train_resnet_4loss_mixmatch(input_t, target_Var, decoders, dec_opts, device,
         onDecOut4_u = torch.cat((onDecOut1_u, temp_t2), dim=1)
 
         #onDecOutT_u = torch.cat((onDecOut1_u, onDecOut2_u, onDecOut3_u), dim=1)
-        """
+        
         for i in range(BATCH_SIZE):
             
             # === Labeled ===
@@ -101,7 +101,7 @@ def train_resnet_4loss_mixmatch(input_t, target_Var, decoders, dec_opts, device,
                                      target_T.contiguous().view(1, 1)), 1))
             
             # === Unlabeled ===
-            """
+            
             # Add L2 loss for unlabeled data (Hierachical)
             unsup_Loss += u_LossFunc(onDecOut1_u[i].view(1, 2), u_mix_label[:,i, :2].contiguous().view(1, 2))
             unsup_Loss += u_LossFunc(onDecOut2_u[i].view(1, 2), u_mix_label[:,i, 2:4].contiguous().view(1, 2))
@@ -112,16 +112,16 @@ def train_resnet_4loss_mixmatch(input_t, target_Var, decoders, dec_opts, device,
             
             # Add L2 loss for unlabeled data
             #unsup_Loss += u_LossFunc(onDecOutT_u[i].view(1, 6), u_mix_label[:,i].contiguous().view(1, 6))
-            """
             
-        print('supervised_Loss: %.10f' % (super_Loss.item() / input_time_step)) #, 'unsupervised_Loss: %.10f' % (unlabel_lambda * unsup_Loss.item() / (unlabel_time_step*unlabel_aug_time)))
-        onLoss = super_Loss # + unlabel_lambda * unsup_Loss
+            
+        print('supervised_Loss: %.10f' % (super_Loss.item() / input_time_step), 'unsupervised_Loss: %.10f' % (unlabel_lambda * unsup_Loss.item() / (unlabel_time_step*unlabel_aug_time)))
+        onLoss = super_Loss + unlabel_lambda * unsup_Loss
         onDecOpt.zero_grad()
         onLoss.backward()
         onDecOpt.step()
         totLoss += onLoss.item()
     
-    return totLoss / input_time_step # total_time_step
+    return totLoss / total_time_step # input_time_step 
 
 def Mixmatch(labeled_data, labeled_label,
              unlabeled_data,
