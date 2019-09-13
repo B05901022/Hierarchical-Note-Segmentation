4974a9adbfe7498c81a724afc9c50ab669534dfe
b05901022 <b05901022@ntu.edu.tw>
2019-09-13 22:32:01 +0800

    VAT_Tree

--- src/train_modules/train_tree_resnet_VAT.py
+++ src/train_modules/train_tree_resnet_VAT.py
@@ -83,23 +83,23 @@ def train_resnet_4loss_VAT_tree(input_t, target_Var, decoders, dec_opts, device,
         # mix_label = torch.cat((x_mix_label, u_mix_label), dim=0)
         
         # --- Run Model ---
-        onDecOut_mix = onDec(torch.cat((x_mix_data, u_mix_data),dim=0)) 
-        #onDecOut6    = onDec(x_mix_data) ###
-        onDecOut6    = F.softmax(onDecOut_mix.view(3,-1,2), dim=2).view(-1,6) ###
+        #onDecOut_mix = onDec(torch.cat((x_mix_data, u_mix_data),dim=0)) 
+        onDecOut6    = onDec(x_mix_data) ###
+        onDecOut6    = F.softmax(onDecOut6.view(3,-1,2), dim=2).view(-1,6) ###
         
         # --- Loss ---        
         # === Supervised Loss ===
-        super_Loss += onLossFunc(torch.log(torch.clamp(ToOneHot(onDecOut6[:BATCH_SIZE]),1e-8)), x_mix_label)
-        super_Loss += sdtLossFunc(onDecOut6[:BATCH_SIZE], curr_Sdt)
+        super_Loss += onLossFunc(torch.log(torch.clamp(ToOneHot(onDecOut6),1e-8)), x_mix_label)
+        super_Loss += sdtLossFunc(onDecOut6, curr_Sdt)
         
         # === Entropy Minimization ===
-        en_Loss    += enLossFunc(ToOneHot(onDecOut6[BATCH_SIZE:]))
+        #en_Loss    += enLossFunc(ToOneHot(onDecOut6[BATCH_SIZE:]))
         
         # === VAT Loss ===
         smsup_Loss += smLossFunc(onDec, u_mix_data)
         
-        print('supervised_Loss: %.10f' % (super_Loss.item() / input_time_step), 'semi-supervised_Loss: %.10f' % (unlabel_lambda * smsup_Loss.item() / input_time_step), 'entropy_Loss: %.10f' % (en_Loss.item() / input_time_step)) #
-        onLoss = super_Loss + unlabel_lambda * smsup_Loss + en_Loss
+        print('supervised_Loss: %.10f' % (super_Loss.item() / input_time_step), 'semi-supervised_Loss: %.10f' % (unlabel_lambda * smsup_Loss.item() / input_time_step)) #'entropy_Loss: %.10f' % (en_Loss.item() / input_time_step)
+        onLoss = super_Loss + unlabel_lambda * smsup_Loss #+ en_Loss
         onDecOpt.zero_grad()
         onLoss.backward()
         onDecOpt.step()
